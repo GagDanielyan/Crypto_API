ec2d0902cc4d9c198718386ea736c8bb9dad6859
GagDanielyan <120093015+GagDanielyan@users.noreply.github.com>
2023-01-31 14:36:44 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import {Route, Routes} from 'react-router-dom'
+import axios from 'axios'
+import Coins from './components/Coins';
+import Coin from './routes/Coin';
+
+import Navbar from './components/Navbar';
+
+function App() {
+  const [coins, setCoins] = useState([]);
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=40&page=1&sparkline=false';
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data)
+      console.log(response)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Coins coins={coins} />}/>
+        <Route path='/coin' element={<Coin />}>
+          <Route path=':coinId' element={<Coin />}/>
+        </Route>
+      </Routes>
+    </>
+  ); 
+}
+
+export default App;
